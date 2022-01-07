89eabcaebc8ea5ddc222bcddaabfd8c4ff3951eb
Keane Chan <e0425601@u.nus.edu>
2022-01-07 23:02:17 +0800

    truncate module info text

--- src/components/ModuleInfo/Information.jsx
+++ src/components/ModuleInfo/Information.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import ShowMoreText from 'react-show-more-text';
 
 const Information = ({ content }) => {
   return (
     <div className="my-2">
       <p className="text-lg font-bold text-forum-title">Module Information</p>
-      <p className="mt-2">{content}</p>
+      <ShowMoreText
+        /* Default options */
+        lines={5}
+        more={<p className="font-semibold text-gray-700">Show more</p>}
+        less={<p className="font-bold text-gray-700">Show less</p>}
+        keepNewLines={false}
+        className="mt-2 text-base"
+        truncatedEndingComponent={'... '}
+      >
+        {content}
+      </ShowMoreText>
     </div>
   );
 };
