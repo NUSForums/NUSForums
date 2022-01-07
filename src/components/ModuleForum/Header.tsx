9dd03471d7b76ea8982ef56db8a1567e6c96cbd3
Keane Chan <e0425601@u.nus.edu>
2022-01-08 01:37:43 +0800

    remove dustbin filter

--- src/components/ModuleForum/Header.tsx
+++ src/components/ModuleForum/Header.tsx
@@ -1,23 +1,10 @@
-import { IoTrash } from 'react-icons/io5';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../hooks/reduxHooks';
-
 import Tag from '../Tag';
 import Dropdown from './Dropdown';
 
 const Header = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
   const metadata = useAppSelector((state) => state.metadata);
   const tagList = metadata.tags;
-
-  const clearFilter = () => {
-    const queryParams = new URLSearchParams(location.search);
-    if (queryParams.has('filter')) queryParams.delete('filter');
-    if (queryParams.has('sort')) queryParams.delete('sort');
-
-    navigate(queryParams.toString());
-  };
 
   return (
     <div className="pt-5">
@@ -32,7 +19,6 @@ const Header = () => {
         </div>
         <div className="flex items-center">
           <Dropdown />
-          <IoTrash size={20} className="cursor-pointer text-forum-title" onClick={clearFilter} />
         </div>
       </div>
     </div>
