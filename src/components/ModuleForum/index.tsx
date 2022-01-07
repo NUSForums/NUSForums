cb87403f127e51859094728f4e4bb486e7d4ce20
Sean <e0406739@u.nus.edu>
2022-01-07 19:02:28 +0800

    insert position for header and create a post

--- src/components/ModuleForum/index.tsx
+++ src/components/ModuleForum/index.tsx
@@ -1,16 +1,23 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useFetchPosts } from '../../hooks';
 import { useAppSelector } from '../../hooks/reduxHooks';
 import ModuleInfo from '../ModuleInfo/ModuleInfo';
+import CreateAPost from './CreateAPost';
+import Header from './Header';
 
 const ModuleForum = () => {
   const { module } = useParams();
   const { posts } = useFetchPosts(module || '');
   const metadata = useAppSelector((state) => state.metadata);
 
+  useEffect(() => {}, []);
+
   return (
     <div className="flex flex-row">
       <div className="flex flex-col flex-grow">
+        <Header />
+        <CreateAPost />
         <pre>{JSON.stringify(metadata, null, 2)}</pre>
         {posts.map((post) => (
           <pre>{JSON.stringify(post, null, 2)}</pre>
